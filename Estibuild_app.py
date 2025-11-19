fdd9d7124ffd122d9e6ec1c4333d27e6fe7d4aa5
MaratheJayesh <108221570+MaratheJayesh@users.noreply.github.com>
2025-11-19 11:03:53 +0530

    Eistbuild_app.py

---
+++ Estibuild_app.py
@@ -0,0 +1,81 @@
+[{
+	"resource": "Untitled-1",
+	"owner": "Pylance1",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"streamlit\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 1,
+	"startColumn": 8,
+	"endLineNumber": 1,
+	"endColumn": 17,
+	"origin": "extHost1"
+},{
+	"resource": "Untitled-1",
+	"owner": "Pylance1",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"utils\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 7,
+	"startColumn": 6,
+	"endLineNumber": 7,
+	"endColumn": 11,
+	"origin": "extHost1"
+},{
+	"resource": "Untitled-1",
+	"owner": "Pylance1",
+	"code": {
+		"value": "reportMissingModuleSource",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingModuleSource.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"pandas\" could not be resolved from source",
+	"source": "Pylance",
+	"startLineNumber": 111,
+	"startColumn": 16,
+	"endLineNumber": 111,
+	"endColumn": 22,
+	"origin": "extHost1"
+},{
+	"resource": "Untitled-1",
+	"owner": "Pylance1",
+	"code": {
+		"value": "reportMissingModuleSource",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingModuleSource.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"pandas\" could not be resolved from source",
+	"source": "Pylance",
+	"startLineNumber": 157,
+	"startColumn": 20,
+	"endLineNumber": 157,
+	"endColumn": 26,
+	"origin": "extHost1"
+}]
